af35c7a864cf3da0f0f5f50c71ddf711abbeca25
Todd Reedy <1851066+treedy@users.noreply.github.com>
2024-01-14 08:29:12 -0500

    Restructure .alias into .alias.d

--- setup.sh
+++ setup.sh
@@ -12,11 +12,12 @@ echo "Make sure to exit once ZSH is installed to continue"
 read -s -k '?Press any key to continue.'
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+backup_file .alias
+backup_file .alias.d
+backup_file .p10k.zsh
+backup_file .tmux.conf
 backup_file .vimrc
 backup_file .zshrc
-backup_file .alias
-backup_file .tmux.conf
-backup_file .p10k.zsh
 
 # Install zsh theme Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -26,11 +27,11 @@ echo "To configure Vim, run the following commands:"
 echo "git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim"
 echo "vim +PluginInstall +qa"
 
-echo "To install NeoVim, run the following commands:"
-echo "wget https://github.com/neovim/neovim/releases/download/v0.9.4/nvim.appimage -O ${HOME}/bin/nvim"
+echo "To install NeoVim <version>, run the following commands:"
+echo "wget https://github.com/neovim/neovim/releases/download/<version>/nvim.appimage -O ${HOME}/bin/nvim"
 echo "chmod +x ${HOME}/bin/nvim"
 echo "git clone git@github.com:treedy/kickstart.nvim.git ~/.config/nvim"
 echo "cd ~/.config/nvim && git checkout personal"
 
 echo ""
-echo "Scrtipt complete!"
+echo "Script complete!"
