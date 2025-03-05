0b12576852953e5210f7a19e0a35bd367719f011
Todd Reedy <1851066+treedy@users.noreply.github.com>
2025-03-05 11:38:07 -0500

    Update setup.sh
    Cleanup and add pre-req checks

--- setup.sh
+++ setup.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
-backup_file() {
-  mv ${HOME}/$1 ${HOME}/$1.backup
+# backup_file() {
+#  mv ${HOME}/$1 ${HOME}/$1.backup
+# }
+
+PRECHECK=0
+precheck() {
+  if ! which "$1" >/dev/null 2>&1; then
+    echo "Please install \'$1\'"
+    PRECHECK=1
+  fi
 }
 
-# https://www.gnu.org/software/stow/manual/stow.html#
-if ! which stow >/dev/null 2>&1; then
-  echo "Please install 'stow'"
+REQS='stow git zsh'
+for req in $REQS; do
+  precheck $req
+done
+if [[ $PRECHECK -eq 1 ]]; then
+  echo "Prerequesite check failed. Install necessary software"
   exit 1
 fi
+# https://www.gnu.org/software/stow/manual/stow.html#
+# if ! which stow >/dev/null 2>&1; then
+#   echo "Please install 'stow'"
+#   exit 1
+# fi
 
 mkdir -p ${HOME}/bin && echo "Created ${HOME}/bin"
 
 # Install Oh My Zsh
-if ! which zsh >/dev/null 2>&1; then
-  echo "Make sure to exit once ZSH is installed to continue"
-  read -s -k '?Press any key to continue.'
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
+# if ! which zsh >/dev/null 2>&1; then
+echo "Make sure to exit once OMZ is installed to continue"
+read -s -k '?Press any key to continue.'
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Backup existing dot files
-for dotfile in dot-*; do 
-  dname="${dotfile/#dot-/.}"
-  backup_file "${dname}"
-done
+# for dotfile in dot-*; do 
+#   dname="${dotfile/#dot-/.}"
+#   backup_file "${dname}"
+# done
 
 # Link all the dotfiles to ~
 stow --dotfiles -t ${HOME} .
@@ -36,15 +51,15 @@ RED="\033[1;31m"
 GREEN="\033[1;32m"
 BLUE="\033[1;34m"
 NOCOLOR="\033[0m"
-# Install vim package manager Vundle
-echo -e "${BLUE}To configure Vim, run the following commands:"
-echo "git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim"
-echo "vim +PluginInstall +qa"
-echo 
-echo "To install NeoVim <version>, run the following commands:"
-echo "wget https://github.com/neovim/neovim/releases/download/<version>/nvim.appimage -O ${HOME}/bin/nvim"
-echo "chmod +x ${HOME}/bin/nvim"
-echo "git clone git@github.com:treedy/kickstart.nvim.git ~/.config/nvim"
-echo "cd ~/.config/nvim && git checkout personal"
+# # Install vim package manager Vundle
+# echo -e "${BLUE}To configure Vim, run the following commands:"
+# echo "git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim"
+# echo "vim +PluginInstall +qa"
+# echo 
+# echo "To install NeoVim <version>, run the following commands:"
+# echo "wget https://github.com/neovim/neovim/releases/download/<version>/nvim.appimage -O ${HOME}/bin/nvim"
+# echo "chmod +x ${HOME}/bin/nvim"
+# echo "git clone git@github.com:treedy/kickstart.nvim.git ~/.config/nvim"
+# echo "cd ~/.config/nvim && git checkout personal"
 echo
-echo -e "${GREEN} Script complete!${NOCOLOR}"
+echo -e "${GREEN} Script complete!${NOCOLOR}. Please restart your session."
